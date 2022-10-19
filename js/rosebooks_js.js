b1db5f73b8331ebfd200201ed6ff049512b58010
Cindy La Rosa <cindylarosamacedo@gmail.com>
2022-10-19 16:16:42 -0300

    Se agrega función formulario

--- js/rosebooks_js.js
+++ js/rosebooks_js.js
@@ -1,6 +1,73 @@
-function(){
-    
-    $.ajax({
-        type:'POST'
-    })
-}
+/*
+document.getElementById("miSelect");
+
+const agregar = () => {
+    const option = document.createElement('option');
+    const valor = new Date().getTime();
+    option.value = valor;
+    option.text = valor;
+    $select.appendChild(option);
+};
+*/
+
+(function () {
+
+    $('#formulario').on("submit", function (e) {
+
+        e.preventDedault();
+
+        var formulario = $(this);
+        var dataSerializada = formulario.serialize();
+
+        console.log(dataSerializada);
+        /*   $.ajax({
+                   type:'GET'
+                   dataType: 'json'
+                   data: dataSerializada
+           })
+           .done(function(data){
+               console.log("Correcto");
+               console.log(data);
+           })
+           .fail(function(){
+               console.log("Fallo");
+                 */
+    });
+
+
+});
+
+
+
+
+$("#botonera a").click(function (event) {
+
+    $("#subir").show("fast");
+
+    event.preventDefault();
+
+    var hash = this.hash;
+
+    $("body").animate({
+
+        scrollTop: $(hash).offset().top
+
+    }, 400);
+
+});
+
+$("#subir a").click(function (event) {
+
+    $("#subir").hide("fast");
+
+    event.preventDefault();
+
+    var hash = this.hash;
+
+    $("body").animate({
+
+        scrollTop: $(hash).offset().top
+
+    }, 400);
+
+});
