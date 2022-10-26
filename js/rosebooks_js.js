eb434a5c20c7fac94f35204071c9307a7ab5c2b6
Cindy La Rosa <cindylarosamacedo@gmail.com>
2022-10-26 09:58:07 -0300

    Se crea función cerrar formulario

--- js/rosebooks_js.js
+++ js/rosebooks_js.js
@@ -1,14 +1,91 @@
 //Función saludo
-function saludo() {
+(function(){
     alert("Bienvenid@ a D'Rose Books");
 
-    saludo();
-  };
+})();
 
 (function(){
 
+    $.box_form = function(callback){
+        
+        var contenido = "";
+            contenido += '<div class="box_form">';
+            contenido += '<button class="btn_cerrar" id="cerrar"><p>X</p></button>';
+            contenido += '<div class="form" id="formulario">';
+            contenido += '<h1>Formulario de solicitud</h1>';
+            contenido += '<h3>Escríbeme y en breve me pondré en contacto contigo</h3>';
+            contenido += '<p>';
+            contenido += '<label for="nombre" class="colocar_nombre">Nombre';
+            contenido += '<span class="obligatorio">*</span>';
+            contenido += '</label>';
+            contenido += '<input type="text" name="introducir_nombre" id="nombre" required="obligatorio" placeholder="Escribe tu nombre">';
+            contenido += '</p>';
+            contenido += '<p>';
+            contenido += '<label for="email" class="colocar_email">Email';
+            contenido += '<span class="obligatorio">*</span>';
+            contenido += '</label>';
+            contenido += '<input type="email" name="introducir_email" id="email" required="obligatorio" placeholder="Escribe tu Email">';
+            contenido += '</p>';
+            contenido += '<p>';
+            contenido += '<label for="categorias">Categoría';
+            contenido += '<span class="obligatorio">*</span>';
+            contenido += '</label>';
+            contenido += '<select name="categoria" id="categoria">';
+            contenido += '<option value="seleccionar"> -- Selecciona una opción --</option>';
+            contenido += '<option value="resena">Presentar reseña de libro</option>';
+            contenido += '<option value="recomendacion">Obtener recomendación de libro</option>';
+            contenido += '<option value="soporte">Problemas de audiolibro</option>';
+            contenido += '</select>';
+            contenido += '</p>';
+            contenido += '<p>';
+            contenido += '<label for="mensaje" class="colocar_mensaje">Mensaje';
+            contenido += '<span class="obligatorio">*</span>';
+            contenido += '</label>';
+            contenido += '<textarea name="introducir_mensaje" class="texto_mensaje" id="mensaje" required="obligatorio"';
+            contenido += 'placeholder="Deja aquí tu comentario..."></textarea>';
+            contenido += '</p>';
+            contenido += '<div class="radio">';
+            contenido += '<p>Seleccionar sexo';
+            contenido += '<span class="obligatorio">*</span>';
+            contenido += '<input type="radio" name="sexo" id="hombre">';
+            contenido += '<label for="hombre">Hombre</label>';
+            contenido += '<input type="radio" name="sexo" id="mujer">';
+            contenido += '<label for="mujer">Mujer</label>';
+            contenido += '</p>';
+            contenido += '</div>';
+            contenido += '<div class="radio">';
+            contenido += '<p>Seleccione idioma';
+            contenido += '<span class="obligatorio">*</span>';
+            contenido += '<input type="radio" name="checkbox" id="es">';
+            contenido += '<label for="espanol">Español</label>';
+            contenido += '<input type="radio" name="checkbox" id="en">';
+            contenido += '<label for="ingles">Inglés</label>';
+            contenido += '</p>';
+            contenido += '</div>';
+            contenido += '<div class="checkbox">';
+            contenido += '<p>';
+            contenido += '<input type="checkbox" name="checkbox" id="termino">';
+            contenido += 'He leído y acepto los términos y condiciones';
+            contenido += '<span class="obligatorio">*</span>';
+            contenido += '</p>';
+            contenido += '</div>';
+            contenido += '<button type="submit" name="enviar_formulario" id="enviar">';
+            contenido += '<p>Enviar</p>';
+            contenido += '</button>';
+            contenido += '<button type="submit" name="cancelar_formulario" id="cancelar">';
+            contenido += '<p>Cancelar</p>';
+            contenido += '</button>';
+            contenido += '<p class="aviso">';
+            contenido += '<span class="obligatorio"> (*) </span>los campos son obligatorios.';
+            contenido += '</p>';
+            contenido += '</div>';
+            contenido += '</div>';
+
+            $("body").append( contenido);
+
     // Función del boton cerrar
-    $("#cerrar").on("click",function(){
+    $(".btn_cerrar").on("click",function(){
+        alert("Boton cerrar");
 
         animar_salida();
 
@@ -22,14 +99,25 @@ function saludo() {
     function animar_salida(){
 
         var $fondo = $(".box_form");
-
-        var $bigBox = $("#cerrar");
+        var $form = $(".btn_cerrar");
 
         var tl = new TimelineMax();
             tl.to( $fondo, 0.5, { opacity: 0} )
-                .to( $bigBox, 1, { opacity: 0, onComplete: remover_bigbox}, "-=0.3" );
+                .to( $form, 1, { opacity: 0, onComplete: remover_form}, "-=0.3" );
 
     }
+    
+    function remover_form(){
+
+        var $fondo = $(".box_form");
+        var $form = $(".btn_cerrar");
+
+        $fondo.remove();
+        $form.remove();
+
+    }
+
+    };
 
 })();
 
